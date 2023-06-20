1138a7aeabe75f58f75303cdb2b8c974bf4bace1
S.Pt.V <1161873455@qq.com>
2023-06-20 17:17:53 +0800

    resolve ip header in thread Listener

--- Models/Monitor.cs
+++ Models/Monitor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.DataProtection;
 using System.Net;
 using System.Net.Sockets;
+using static DNSmonitor.Models.Headers;
 
 namespace DNSmonitor.Models
 {
@@ -34,7 +35,31 @@ namespace DNSmonitor.Models
         /// </summary>
         private Thread Listener;
         private bool keeplistening;
-        
+
+        /// <summary>
+        /// 处理接收到的数据包的线程
+        /// </summary>
+        private Thread Process;
+        private bool keepprocessing;
+        private bool canprocess
+        {
+            get
+            {
+                if (packets.Count > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            set
+            {
+                canprocess = value;
+            }
+        }
+
         /// <summary>
         /// 发送syslog的线程
         /// </summary>
@@ -69,7 +94,18 @@ namespace DNSmonitor.Models
                 RawsocketListen();
             });
             Listener = new Thread(ListenerStart);
-            
+
+            // 处理线程设置
+            keepprocessing = true;
+            ParameterizedThreadStart? ProcessStart = new((obj) =>
+            {
+                while (keepprocessing)
+                {
+                    PacketProcess();
+                }
+            });
+            Process = new Thread(ProcessStart);
+
             // syslog线程设置
             ParameterizedThreadStart? SyslogStart = new((obj) =>
             {
@@ -90,6 +126,8 @@ namespace DNSmonitor.Models
         /// </summary>
         public void Run()
         {
+            // Process.Start();
+            // Console.WriteLine("Thread Process started: " + Listener.ManagedThreadId.ToString());
             Listener.Start();
             Console.WriteLine("Thread Listener started: " + Listener.ManagedThreadId.ToString());
         }
@@ -144,7 +182,70 @@ namespace DNSmonitor.Models
                 if (rawsocket != null)
                 {
                     int recved = rawsocket.Receive(recv_buffer);
-                    Console.WriteLine(recved.ToString());
+                    Console.WriteLine("recved " + recved.ToString() + " bytes");
+                    byte[] packet = new byte[recved];
+                    Array.Copy(recv_buffer, 0, packet, 0, recved);
+                    
+                    // packets.Add(packet);
+                    // Console.WriteLine(packets.Count.ToString());
+
+                    fixed(byte* fixed_buf = packet)
+                    {
+                        IPHeader* header = (IPHeader*)fixed_buf;
+                        uint version = (uint)(header->ip_verlen & 0xF0) >> 4;
+                        uint length = (uint)(header->ip_verlen & 0x0F) << 2;
+                        byte tos = (byte)header->ip_tos;
+                        ushort totallength = (ushort)(header->ip_totallength);
+                        ushort identification = (ushort)header->ip_id;
+                        ushort offset = (ushort)header->ip_offset;
+                        byte ttl = (byte)header->ip_ttl;
+                        byte protocol_byte = (byte)header->ip_protocol;
+                        ushort checksum = (ushort)header->ip_checksum;
+                        string src_addr = new IPAddress(header->ip_srcaddr).ToString();
+                        string dst_addr = new IPAddress(header->ip_dstaddr).ToString();
+
+                        string protocol = "";
+                        switch (protocol_byte)
+                        {
+                            case 1:
+                                protocol = "ICMP";
+                                break;
+                            case 2:
+                                protocol = "IGMP";
+                                break;
+                            case 6:
+                                protocol = "TCP";
+                                break;
+                            case 17:
+                                protocol = "UDP";
+                                break;
+                            default:
+                                protocol = "UNKONOWN";
+                                break;
+                        }
+
+                        // uint src_addr = header->ip_srcaddr;
+                        // uint dst_addr = header->ip_dstaddr;
+                        Console.WriteLine(src_addr + " to " + dst_addr + " " + protocol);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 数据包处理
+        /// </summary>
+        private void PacketProcess()
+        {
+            while (true)
+            {
+
+                if (canprocess)
+                {
+                    int length = packets[0].Length;
+                    Console.WriteLine("first packet length " + length.ToString());
+                    Console.WriteLine(packets.Count.ToString());
+                    packets.RemoveAt(0);
                 }
             }
         }
