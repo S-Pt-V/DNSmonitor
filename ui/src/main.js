56c4e82aa1335949f65f2eae11bf7cd46d0a3a6e
S.Pt.V <1161873455@qq.com>
2023-08-14 16:07:00 +0800

    add axios

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,10 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Axios from "@/plugins/axios";
+import qs from "qs";
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = Axios;
+Vue.prototype.$qs = qs;
 
 new Vue({
+  Axios,
   router,
   store,
   vuetify,
